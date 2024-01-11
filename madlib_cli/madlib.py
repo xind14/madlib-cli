48a9bdb0b53abc5176a691a720524f089bf15ed1
Xin Deng <xind1470@gmail.com>
2024-01-10 19:50:27 -0800

    madlib generation function works

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,4 +1,6 @@
-print("Current working directory:", os.getcwd())
+# import os
+
+# print("Current working directory:", os.getcwd())
 
 def welcome_message ():
     print('''
@@ -9,8 +11,8 @@ def welcome_message ():
     
 welcome_message()
 
-videogame_path = 'assets/make_me_a_video_game_template.txt'
-# stormy_path = './assets/dark_and_stormy_night_template.txt'
+stormy_path = './assets/dark_and_stormy_night_template.txt'
+videogame_path = './assets/make_me_a_video_game_template.txt'
 
 def read_template(file_path):
     try:
@@ -22,21 +24,21 @@ def read_template(file_path):
 
 def parse_template(template):
     parts_of_speech = []
-    print(parts_of_speech)
+    # print(parts_of_speech)
     split_template = template.split('{')
-    print("split_template:", split_template)
+    # print("split_template:", split_template)
 
     for i in range(1, len(split_template)):
         singular_speech, remaining_char = split_template[i].split('}', 1)
-        print("part:", singular_speech)
-        print("rest:", remaining_char)
+        # print("part:", singular_speech)
+        # print("rest:", remaining_char)
         parts_of_speech.append(singular_speech)
-        print(parts_of_speech)
+        # print(parts_of_speech)
         split_template[i] = remaining_char
-        print(split_template)
+        # print(split_template)
 
     stripped_template = '{}'.join(split_template)
-    print("stripped_template:", stripped_template)
+    # print("stripped_template:", stripped_template)
 
 
     return stripped_template, tuple(parts_of_speech)
@@ -50,6 +52,20 @@ def merge(stripped_template, parts_of_speech):
     return result
 
 
-# Create and test a merge function that takes in a “bare” template and a list of user entered language parts, and returns a string with the language parts inserted into the template.
 
 
+def madlib_generator():
+
+    template = read_template(videogame_path)
+    stripped_template, parts_of_speech = parse_template(template)
+    user_input = []
+    for word in parts_of_speech:
+        word_input = input(f"\nEnter {word} > ")
+        user_input.append(word_input)
+    final_madlib = merge(stripped_template, user_input)
+    print(f"\n\n\nHere is you completed Madlib: \n\n{final_madlib}\n")
+
+    with open ('assets/final_madlib.txt', 'w') as file:
+        file.write(final_madlib)
+
+madlib_generator()
